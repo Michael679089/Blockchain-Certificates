8e099aaaf019a3dfd1def19a8596d75e94655fd9
Michael <purplecheriandres@gmail.com>
2025-11-10 02:33:44 +0800

    Leaving a copy of Test1 to main since it's stable

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite"; // For Designs Deployment
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel"; // For Vercel Website Deployment
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  adapter: vercel()
+  adapter: vercel(),
 });
